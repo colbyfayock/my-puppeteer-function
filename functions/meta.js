829ed59a6c369a1dc10975f40cf8bc54e5e21613
colbyfayock <fayock@gmail.com>
2021-08-25 23:42:25 -0400

    Step 4: creating a browser instance and finding the title and description

--- functions/meta.js
+++ functions/meta.js
@@ -1,8 +1,30 @@
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
 exports.handler = async function(event, context) {
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath,
+    headless: true,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto('https://spacejelly.dev/');
+
+  const title = await page.title();
+  const description = await page.$eval('meta[name="description"]', element => element.content);
+
+  await browser.close();
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      status: 'Ok'
+      status: 'Ok',
+      page: {
+        title,
+        description
+      }
     })
   };
 }
